967e666fbfc783a630a821c00d85364e0aa2e378
Matt de Waard <dewaardmatt@gmail.com>
2025-03-15 09:18:39 +1300

    Adds initial project setup

---
+++ MusicApp/MusicApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  MusicApp
+//  Created by Matt de Waard on 15/03/2025.
+//  Copyright © 2025. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicApp/MusicApp/MusicAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  MusicApp
+//  Created by Matt de Waard on 15/03/2025.
+//  Copyright © 2025. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
